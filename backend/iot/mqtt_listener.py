fc392c0c27b759dfc4d720850c73261f48260c37
Zayd <zaydbenfathel@gmail.com>
2025-06-26 23:53:43 +0100

    Fixed the IoT change & added cronjob for IoT for automation

--- backend/iot/mqtt_listener.py
+++ backend/iot/mqtt_listener.py
@@ -5,6 +5,7 @@ import paho.mqtt.client as mqtt
 from datetime import datetime
 from .models import IotData
 from dotenv import load_dotenv
+import time
 # --- Setup Django Environment ---
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
@@ -37,7 +38,7 @@ def on_message(client, userdata, msg):
             data = payload.get("data"),
             rx_info = payload.get("rxInfo"),
             object_json = object_data,
-            light_lux = object_data.get("analogInput", {}).get("5"),
+            light_lux = object_data.get("illuminanceSensor", {}).get("5"),
             ch4_ppm = object_data.get("analogInput", {}).get("6"),
             co2_ppm = object_data.get("analogInput", {}).get("7"),
             barometer = object_data.get("barometer", {}).get("4"),
@@ -60,5 +61,8 @@ mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
 mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
-print("[System] Starting MQTT loop...")
-mqtt_client.loop_forever()
+print("[System] Starting MQTT loop for 30 seconds...")
+mqtt_client.loop_start()
+time.sleep(30)  # Ecoute pendant 30 secondes
+mqtt_client.loop_stop()
+print("[System] Exiting.")
